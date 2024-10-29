2aed695af5d051c553fdb03ad867dce21af1d13c
Alexandre Pinto <alexandrepp07@gmail.com>
2024-10-29 19:50:48 +0000

    Create models and admin
    Created an admin user and new models such as Athlete, Coach, Group, AssociateAthleteGroup and AssociateCoachGroup.

--- backend/trainingmanagement/admin.py
+++ backend/trainingmanagement/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Athlete, Coach
 # Register your models here.
+admin.site.register(Athlete)
+admin.site.register(Coach)
--- backend/trainingmanagement/models.py
+++ backend/trainingmanagement/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Athlete(models.Model):
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=200)
+    phoneNumber = models.IntegerField()
+    email = models.CharField(max_length=100)
+    birthday = models.DateField()
+
+class Coach(models.Model):
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=200)
+    phoneNumber = models.IntegerField()
+    email = models.CharField(max_length=100)
+    birthday = models.DateField()
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+
+class AssociateAthleteGroup(models.Model):
+    athleteId = models.ForeignKey(Athlete, on_delete=models.CASCADE)
+    groupId = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+class AssociateCoachGroup(models.Model):
+    athleteId = models.ForeignKey(Coach, on_delete=models.CASCADE)
+    groupId = models.ForeignKey(Group, on_delete=models.CASCADE)
